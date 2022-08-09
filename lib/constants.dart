da3cd95d480bee2af6321487c8d3c0942cc41298
Naimehoosein77 <naimehossein77@gmail.com>
2022-08-09 18:36:48 +0600

    add constant file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,19 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+late Size size;
+double zoom = 15;
+
+final Color black = Colors.black;
+final Color white = Colors.white;
+final Color amber = Colors.amber;
+final Color red = Colors.red;
+final Color grey = Colors.grey;
+final Color blue = Colors.blue;
+final Color transparent = Colors.transparent;
+final Color textFieldColor = Color(0xffEEEEEE);
+
+kLog(String value) {
+  log(value);
 }
