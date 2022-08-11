61185f4d66c3b132a93c8268d1b742c85cceb4ab
Iftekharul Islam <iftekharul.tamim@gmail.com>
2022-08-11 12:00:07 +0600

    added K front of constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,14 +5,14 @@ import 'package:flutter/material.dart';
 late Size size;
 double zoom = 15;
 
-final Color black = Colors.black;
-final Color white = Colors.white;
-final Color amber = Colors.amber;
-final Color red = Colors.red;
-final Color grey = Colors.grey;
-final Color blue = Colors.blue;
-final Color transparent = Colors.transparent;
-final Color textFieldColor = Color(0xffEEEEEE);
+final Color kblack = Colors.black;
+final Color kwhite = Colors.white;
+final Color kamber = Colors.amber;
+final Color kred = Colors.red;
+final Color kgrey = Colors.grey;
+final Color kblue = Colors.blue;
+final Color ktransparent = Colors.transparent;
+final Color ktextFieldColor = Color(0xffEEEEEE);
 
 final String kAppName = 'Freelancer app';
 
