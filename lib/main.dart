fd3a435e9c3285ba01d737dd38b067460b4d473f
Mahmudul Hasan <mahmudulhasan5008@gmail.com>
2023-02-05 12:26:27 +0600

    for stagging

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
         scaffoldBackgroundColor: kscaffoldBackgroundColor,
       ),
-      initialRoute: Routes.directionsPageRoute,
+      initialRoute: Routes.homePageRoute,
       getPages: AppPages.pages,
     );
   }
